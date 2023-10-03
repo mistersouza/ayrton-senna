ce3c9f1c1886cb3543b68cae33a59cd4102b8bce
mistersouza <>
2023-10-03 13:26:46 +0000

    Document stylesheet

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const links = document.querySelectorAll("nav a");
+const sections = document.querySelectorAll('section');
 
 links.forEach((link) => {
     link.addEventListener("click", () => {
-        const navToggle = document.getElementById("nav-toggle");
-        if (navToggle.checked) {
-            navToggle.checked = false;
-        }
+        const nav = document.querySelector("#nav-toggle");
+        if (nav.checked) nav.checked = false;
     });
 });
+
+
+window.onscroll = () => {
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            links.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            // Use document.querySelector to select the link by href attribute
+            let linkToActivate = document.querySelector('nav a[href="#' + id + '"]');
+            if (linkToActivate) linkToActivate.classList.add('active');
+        }
+    });
+};
